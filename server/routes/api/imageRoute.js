d3f6617378ef9d7e1947ad9fed6d78085a8062b4
Sean Genge <seangenge@gmail.com>
2022-10-31 21:42:19 +1100

    Trying to fix problem with heroku and image deployment

--- server/routes/api/imageRoute.js
+++ server/routes/api/imageRoute.js
@@ -6,7 +6,17 @@ const path = require('path');
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
 		// This folder has to exist if not this will throw an error
-		cb(null, path.join(__dirname, '../../../client/public/images/uploads'));
+		if (process.env.NODE_ENV === "production") {
+			// Create the upload directory for uploading images in the build folder if it doesn't exist
+			if (!fs.existsSync(path.join(__dirname, '../../../client/build/images/uploads'))) {
+				fs.mkdirSync(path.join(__dirname, '../../../client/build/images/uploads'), { recursive: true });
+			}
+			
+			cb(null, path.join(__dirname, '../../../client/build/images/uploads'));
+		}
+		else {
+			cb(null, path.join(__dirname, '../../../client/public/images/uploads'));
+		}
 	},
 	filename: function (req, file, cb) {
 		const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
@@ -22,11 +32,19 @@ const upload = multer({ storage: storage });
 router.post('/upload', upload.single('dogPic'), function (req, res) {
 	console.log(req.file, req.body);
 	console.log("File received: ", req.file.filename);
+	let fileLoc = '';
+	
+	if (process.env.NODE_ENV === "production") {
+		fileLoc = "/build/images/uploads/";
+	}
+	else {
+		fileLoc = "/images/uploads/";
+	}
 	
 	const file = {
 		filePath: `/public/images/uploads/${req.file.filename}`,
 		fileName: req.file.filename,
-		publicFilePath: `/images/uploads/${req.file.filename}`
+		publicFilePath: `${fileLoc}${req.file.filename}`
 	};
 	
 	res.json(file);
