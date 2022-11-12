687f42b0cdb59af5027994d49c2bb45833a16a7a
Sean Genge <seangenge@gmail.com>
2022-11-12 17:41:56 +1100

    Fixed refreshing pages

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const path = require("path");
 
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-	return res.json('Wrong route!');
+	return res.sendFile(path.join(__dirname, "../../../client/public/index.html"));
 });
 
 module.exports = router;
